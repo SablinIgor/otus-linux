84063d355a03daa34d41aaa71e54c7ef653172f3
Igor <sablin.igor@gmail.com>
2019-11-27 00:58:06 -0500

    Lock protection

---
+++ HW6-bash/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function setValue {
+  echo "SetKey function was colled wyth key $1!"
+}
+
+function getValue {
+  echo "GetKey function was colled with key $1!"
+}
+
+getValue 1100
+
+
